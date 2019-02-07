2a6816a5629c45284315b24f35fbb8ad51486c52
Cody-Price <ctprice11@gmail.com>
2019-02-06 19:05:19 -0700

    add server

---
+++ build/precache-manifest.85a76278367bff1739f8b3f8234b5346.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fea8c6b27d2de4a476a",
+    "url": "/static/css/main.996b99bd.chunk.css"
+  },
+  {
+    "revision": "5fea8c6b27d2de4a476a",
+    "url": "/static/js/main.5fea8c6b.chunk.js"
+  },
+  {
+    "revision": "ef40115bc155a27abd59",
+    "url": "/static/js/1.ef40115b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35d84a953cf5a258e95f0d642bd6d864",
+    "url": "/static/media/post-beer.35d84a95.png"
+  },
+  {
+    "revision": "f0834631c4889c237e8436b8cf7b7b6c",
+    "url": "/static/media/delete-beer.f0834631.png"
+  },
+  {
+    "revision": "92e8b12c27ab877236f2b73147054b7c",
+    "url": "/static/media/patch-beer.92e8b12c.png"
+  },
+  {
+    "revision": "6a0fd28cf0892ca0aed1734522dd4af6",
+    "url": "/static/media/put-beer.6a0fd28c.png"
+  },
+  {
+    "revision": "33912730d2f4beb39db084d7ac53b0c2",
+    "url": "/static/media/put-brew.33912730.png"
+  },
+  {
+    "revision": "293d8c8470868d03a57bbfc72fe1f152",
+    "url": "/static/media/patch-brew.293d8c84.png"
+  },
+  {
+    "revision": "58f102bdfae3344e553149f1d7491897",
+    "url": "/static/media/post-brew.58f102bd.png"
+  },
+  {
+    "revision": "993db81479b2d03e3468052b113390bd",
+    "url": "/static/media/delete-brew.993db814.png"
+  },
+  {
+    "revision": "456b444e7067f380d16dc2712c7fa4a2",
+    "url": "/index.html"
+  }
+];
